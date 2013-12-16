f349683be94410368bbb176414f44d8bed3e9f75
Thein Htike Aung <thein.thein99@gmail.com>
2013-12-17 01:16:34 +0800

    - Test ActionProvider

--- Megamind/src/com/theinhtikeaung/megamind/MainActivity.java
+++ Megamind/src/com/theinhtikeaung/megamind/MainActivity.java
@@ -1,24 +1,62 @@
 package com.theinhtikeaung.megamind;
 
+import android.R.anim;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.activity_main);
-		
-		
 	}
+
+	private ShareActionProvider mShareActionProvider;
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		
+		// Set up ShareActionProvider's default share intent
+	    MenuItem shareItem = menu.findItem(R.id.actoin_search);
+	    mShareActionProvider = (ShareActionProvider)MenuItemCompat.getActionProvider(shareItem);
+	    mShareActionProvider.setShareIntent(getDefaultIntent());
+		
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	/** Defines a default (dummy) share intent to initialize the action provider.
+	  * However, as soon as the actual content to be used in the intent
+	  * is known or changes, you must update the share intent by again calling
+	  * mShareActionProvider.setShareIntent()
+	  */
+	private Intent getDefaultIntent() {
+	    Intent intent = new Intent(Intent.ACTION_SEND);
+	    intent.setType("image/*");
+	    return intent;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case R.id.actoin_search:
+	        	//Toast.makeText(this, "Search Menu is selected", Toast.LENGTH_LONG).show();
+	        	startActivity(new Intent(this, SecondActivity.class));
+	            return true;
+	        case R.id.action_phone:
+	            //composeMessage();
+	        	Toast.makeText(this, "Phone Menu is selected", Toast.LENGTH_LONG).show();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 }
---
+++ Megamind/src/com/theinhtikeaung/megamind/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.theinhtikeaung.megamind;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+
+public class SecondActivity extends ActionBarActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+		
+		ActionBar actionBar = getSupportActionBar();
+	    actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+}
